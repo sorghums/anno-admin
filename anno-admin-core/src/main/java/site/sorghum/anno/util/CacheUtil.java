9e220ae6959a68d4cda901b6bf9a9a3781a7f57c
Sorghum <sorghum2021@163.com>
2023-06-28 10:31:20 +0800

    优化部分逻辑。

---
+++ anno-admin-core/src/main/java/site/sorghum/anno/util/CacheUtil.java
@@ -0,0 +1,71 @@
+package site.sorghum.anno.util;
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.annotation.Init;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.annotation.ProxyComponent;
+import org.noear.solon.data.annotation.CacheRemove;
+import org.redisson.api.RMapCache;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.JacksonCodec;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+/**
+ * 缓存工具类
+ *
+ * @author Sorghum
+ * @since 2023/06/28
+ */
+@Component
+public class CacheUtil {
+
+    private static RedissonClient redissonClient;
+
+    private static final String CACHE_PREFIX = "anno:cache";
+
+    @Init
+    public void init() {
+        Solon.context().getBeanAsync(
+                RedissonClient.class,
+                (bean) -> {
+                    redissonClient = bean;
+                }
+        );
+    }
+
+    public static <T> T getOrCreate(String sourceName, Supplier<T> tSupplier, Class<T> clazz, Long cacheSeconds, String... args) {
+        String mapKey = buildKey(true, sourceName);
+        String memberKey = buildKey(false, args);
+        RMapCache<String, String> cache = redissonClient.getMapCache(mapKey, new StringCodec());
+        if (cache.containsKey(memberKey)) {
+            String value = cache.get(memberKey);
+            return JSONUtil.parseObject(value, clazz);
+        }
+        T t = tSupplier.get();
+        cache.put(memberKey, JSONUtil.toJSONString(t), cacheSeconds, TimeUnit.SECONDS);
+        return t;
+    }
+
+    public static void remove(String sourceName, String... args) {
+        String mapKey = buildKey(true, sourceName);
+        String memberKey = buildKey(false, args);
+        RMapCache<String, String> cache = redissonClient.getMapCache(mapKey, new StringCodec());
+        cache.remove(memberKey);
+    }
+
+    private static String buildKey(boolean prefix, String... args) {
+        StringBuilder sb;
+        if (prefix) {
+            sb = new StringBuilder(CACHE_PREFIX);
+        }
+        sb = new StringBuilder();
+        for (String arg : args) {
+            sb.append(":").append(arg);
+        }
+        return sb.toString();
+    }
+}
