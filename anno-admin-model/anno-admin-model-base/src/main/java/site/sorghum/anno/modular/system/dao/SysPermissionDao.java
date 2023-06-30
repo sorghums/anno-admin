9c12bead4a950666e3af4f36b68f2e5ae6d161cb
Sorghum <sorghum2021@163.com>
2023-06-30 16:03:34 +0800

    增加部分注释，修改readme

--- anno-admin-model/anno-admin-model-base/src/main/java/site/sorghum/anno/modular/system/dao/SysPermissionDao.java
+++ anno-admin-model/anno-admin-model-base/src/main/java/site/sorghum/anno/modular/system/dao/SysPermissionDao.java
@@ -16,9 +16,20 @@ import java.util.List;
  */
 @Namespace("site.sorghum.anno.modular.system.dao")
 public interface SysPermissionDao extends BaseMapper<SysPermission> {
+    /**
+     * 根据用户id查询系统权限
+     *
+     * @param uid 用户id
+     * @return {@link List}<{@link SysPermission}>
+     */
     @Sql("select * from sys_permission where id in (select permission_id from sys_role_permission where role_id in (select role_id from sys_user_role where user_id = ?  and del_flag = 0 ) and del_flag = 0)")
     List<SysPermission> querySysPermissionByUserId(String uid);
 
+    /**
+     * 列表
+     *
+     * @return {@link List}<{@link SysPermission}>
+     */
     @Sql("select * from sys_permission where del_flag = 0")
     List<SysPermission> list();
 }
--- anno-admin-model/anno-admin-model-base/src/main/java/site/sorghum/anno/modular/system/dao/SysRoleDao.java
+++ anno-admin-model/anno-admin-model-base/src/main/java/site/sorghum/anno/modular/system/dao/SysRoleDao.java
@@ -16,6 +16,12 @@ import java.util.List;
  */
 @Namespace("site.sorghum.anno.modular.system.dao")
 public interface SysRoleDao extends BaseMapper<SysRole> {
+    /**
+     * 通过用户id查询系统角色
+     *
+     * @param uid 用户id
+     * @return {@link List}<{@link SysRole}>
+     */
     @Sql("select * from sys_role where id in (select role_id from sys_user_role where user_id = ? and del_flag = 0 ) and del_flag = 0")
     List<SysRole> querySysRoleByUserId(String uid);
 }
