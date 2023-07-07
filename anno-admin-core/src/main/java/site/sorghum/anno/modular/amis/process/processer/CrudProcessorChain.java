81af594e2d8be7d100505a1b01867bdb1ed443f3
Sorghum <sorghum2021@163.com>
2023-07-07 17:25:16 +0800

    优化链式驱动。

--- anno-admin-core/src/main/java/site/sorghum/anno/modular/amis/process/processer/CrudProcessorChain.java
+++ anno-admin-core/src/main/java/site/sorghum/anno/modular/amis/process/processer/CrudProcessorChain.java
@@ -16,23 +16,35 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @since 2023/07/07
  */
 public class CrudProcessorChain implements BaseProcessorChain {
+    /**
+     * 处理器索引
+     */
     private final AtomicInteger index = new AtomicInteger(0);
-    List<BaseProcessor> processors = List.of(
+    private static final List<BaseProcessor> PROCESSORS = List.of(
+            // 初始化
             new CrudViewInitProcessor(),
+            // 增删改查的列信息
             new CrudColumnProcessor(),
+            // 过滤的表单信息
             new CrudFilterProcessor(),
+            // 每一行的删除信息
             new CrudDeleteBtnProcessor(),
+            // 每一行的编辑信息
             new CrudEditInfoProcessor(),
+            // 每一行的自定义按钮信息
             new CrudColumnButtonProcessor(),
+            // 每一行的新增信息
             new CrudAddInfoProcessor(),
+            // 边栏的树信息
             new CrudTreeAsideProcessor(),
+            // 多对多的多选框信息
             new CrudM2mCheckProcessor()
 
     );
     @Override
     public void doProcessor(AmisBaseWrapper amisBaseWrapper, Class<?> clazz, Map<String, Object> properties){
-        if (index.get() < processors.size()) {
-            processors.get(index.getAndIncrement()).doProcessor(amisBaseWrapper, clazz, properties, this);
+        if (index.get() < PROCESSORS.size()) {
+            PROCESSORS.get(index.getAndIncrement()).doProcessor(amisBaseWrapper, clazz, properties, this);
         }
     }
 }
