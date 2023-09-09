d975c8a62c1db9c8a176aad90dbe672709eda615
songyinyin <song_yinyin@163.com>
2023-09-09 11:07:11 +0800

    refactor: 优化获取 optionData 代码

--- anno-admin/src/main/java/site/sorghum/anno/amis/type/parser/OptionsTypeParser.java
+++ anno-admin/src/main/java/site/sorghum/anno/amis/type/parser/OptionsTypeParser.java
@@ -1,27 +1,16 @@
 package site.sorghum.anno.amis.type.parser;
 
 import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.ReflectUtil;
-import cn.hutool.core.util.StrUtil;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import lombok.SneakyThrows;
-import org.noear.wood.DbContext;
-import org.noear.wood.annotation.Db;
 import site.sorghum.amis.entity.AmisBase;
 import site.sorghum.amis.entity.display.Mapping;
 import site.sorghum.amis.entity.input.FormItem;
 import site.sorghum.amis.entity.input.Options;
-import site.sorghum.anno._metadata.AnEntity;
+import site.sorghum.amis.entity.input.Picker;
 import site.sorghum.anno._metadata.AnField;
-import site.sorghum.anno._metadata.MetadataManager;
 import site.sorghum.anno.amis.type.TypeParser;
-import site.sorghum.anno.anno.proxy.DbServiceWithProxy;
+import site.sorghum.anno.amis.util.AmisCommonUtil;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,85 +23,20 @@ import java.util.Map;
 @Named
 public class OptionsTypeParser implements TypeParser {
 
-    @Db
-    DbContext dbContext;
-
-    @Inject
-    DbServiceWithProxy dbServiceWithProxy;
-
-    @Inject
-    MetadataManager metadataManager;
-
-    @SneakyThrows
     @Override
     public Map<String, Object> parseDisplay(AmisBase amisBase, AnField anField) {
         Mapping mappingItem = new Mapping();
-        HashMap<Object, Object> mapping = new HashMap<>();
-        String optionTypeSql = anField.getOptionTypeSql();
-        AnField.OptionAnnoClass optionAnnoClass = anField.getOptionAnnoClass();
-        if (StrUtil.isNotBlank(optionTypeSql)) {
-            List<Map<String, Object>> mapList = dbContext.sql(optionTypeSql).getDataList().getMapList();
-            for (Map<String, Object> map : mapList) {
-                mapping.put(map.get("value").toString(), map.get("label"));
-            }
-        } else if(!optionAnnoClass.getAnnoClass().equals(Object.class)){
-            AnEntity anEntity = metadataManager.getEntity(optionAnnoClass.getAnnoClass());
-            List<?> dataList = dbServiceWithProxy.list(
-                anEntity.getClazz(),
-                new ArrayList<>()
-            );
-            for (Object data : dataList) {
-                mapping.put(
-                    ReflectUtil.getFieldValue(data, optionAnnoClass.getIdKey()),
-                    ReflectUtil.getFieldValue(data, optionAnnoClass.getLabelKey())
-                );
-            }
-        } else if (CollUtil.isNotEmpty(anField.getOptionDatas())){
-            for (AnField.OptionData optionData : anField.getOptionDatas()) {
-                mapping.put(optionData.getValue(), optionData.getLabel());
-            }
-        }
+        Map<Object, Object> mapping = AmisCommonUtil.getOptionData(anField);
         mappingItem.setMap(mapping);
         return mergeObj(mappingItem, amisBase);
     }
 
-    @SneakyThrows
     @Override
     public FormItem parseEdit(FormItem formItem, AnField anField) {
-        Options options = new Options();
-        BeanUtil.copyProperties(formItem, options);
-        List<Options.Option> optionItemList = new ArrayList<>();
-        String optionTypeSql = anField.getOptionTypeSql();
-        AnField.OptionAnnoClass optionAnnoClass = anField.getOptionAnnoClass();
-        if (StrUtil.isNotBlank(optionTypeSql)) {
-            List<Map<String, Object>> mapList = dbContext.sql(optionTypeSql).getDataList().getMapList();
-            for (Map<String, Object> map : mapList) {
-                Options.Option optionItem = new Options.Option();
-                optionItem.setLabel(MapUtil.getStr(map, "label"));
-                optionItem.setValue(map.get("value"));
-                optionItemList.add(optionItem);
-            }
-        } else if(!optionAnnoClass.getAnnoClass().equals(Object.class)){
-            AnEntity anEntity = metadataManager.getEntity(optionAnnoClass.getAnnoClass());
-            List<?> dataList = dbServiceWithProxy.list(
-                anEntity.getClazz(),
-                new ArrayList<>()
-            );
-            for (Object data : dataList) {
-                Options.Option optionItem = new Options.Option();
-                optionItem.setLabel(StrUtil.toString(ReflectUtil.getFieldValue(data, optionAnnoClass.getLabelKey())));
-                optionItem.setValue(StrUtil.toString(ReflectUtil.getFieldValue(data, optionAnnoClass.getIdKey())));
-                optionItemList.add(optionItem);
-            }
-        } else if (CollUtil.isNotEmpty(anField.getOptionDatas())){
-            for (AnField.OptionData optionData : anField.getOptionDatas()) {
-                Options.Option optionItem = new Options.Option();
-                optionItem.setLabel(optionData.getLabel());
-                optionItem.setValue(optionData.getValue());
-                optionItemList.add(optionItem);
-            }
-        }
-        options.setOptions(optionItemList);
-        return options;
+        Picker picker = new Picker();
+        BeanUtil.copyProperties(formItem, picker);
+        List<Options.Option> optionItemList = AmisCommonUtil.getOptions(anField);
+        picker.setOptions(optionItemList);
+        return picker;
     }
 }
--- anno-admin/src/main/java/site/sorghum/anno/amis/util/AmisCommonUtil.java
+++ anno-admin/src/main/java/site/sorghum/anno/amis/util/AmisCommonUtil.java
@@ -1,30 +1,44 @@
 package site.sorghum.anno.amis.util;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import org.noear.wood.DbContext;
 import site.sorghum.amis.entity.AmisBase;
 import site.sorghum.amis.entity.display.Group;
 import site.sorghum.amis.entity.input.FormItem;
+import site.sorghum.amis.entity.input.Options;
+import site.sorghum.anno._common.AnnoBeanUtils;
+import site.sorghum.anno._common.AnnoConstants;
+import site.sorghum.anno._common.exception.BizException;
+import site.sorghum.anno._metadata.AnEntity;
+import site.sorghum.anno._metadata.AnField;
+import site.sorghum.anno.anno.proxy.DbServiceWithProxy;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * AMIS常用方法集合
+ * AMIS 常用方法集合
+ *
  * @author Sorghum
  * @since 2023/09/08
  */
 public class AmisCommonUtil {
+
     public static List<AmisBase> formItemToGroup(List<AmisBase> formItems, Integer columnCnt) {
         String[] ignoreTypes = {
-            "editor","input-rich-text","input-image"
+            "editor", "input-rich-text", "input-image"
         };
         // 加入传入的忽略类型
-        Map<Integer,List<AmisBase>> indexMap = new HashMap<>();
+        Map<Integer, List<AmisBase>> indexMap = new HashMap<>();
         List<AmisBase> rst = new ArrayList<>();
         int idx = 0;
         for (AmisBase formItem : formItems) {
-            if (Boolean.TRUE.equals(formItem.getHidden())){
+            if (Boolean.TRUE.equals(formItem.getHidden())) {
                 continue;
             }
             List<AmisBase> nowIdxFormItems = indexMap.computeIfAbsent(idx, k -> new ArrayList<>());
@@ -36,7 +50,7 @@ public class AmisCommonUtil {
             boolean skip = false;
             for (String ignoreType : ignoreTypes) {
                 if (ignoreType.equals(formItem.getType())) {
-                    if (!nowIdxFormItems.isEmpty()){
+                    if (!nowIdxFormItems.isEmpty()) {
                         idx++;
                         nowIdxFormItems = indexMap.computeIfAbsent(idx, k -> new ArrayList<>());
                     }
@@ -50,7 +64,7 @@ public class AmisCommonUtil {
             }
             // 如果不是formItem则单独放到一个组中
             if (!(formItem instanceof FormItem)) {
-                if (!nowIdxFormItems.isEmpty()){
+                if (!nowIdxFormItems.isEmpty()) {
                     idx++;
                     nowIdxFormItems = indexMap.computeIfAbsent(idx, k -> new ArrayList<>());
                 }
@@ -68,6 +82,56 @@ public class AmisCommonUtil {
             rst.add(group);
         }
         return rst;
+    }
+
+    /**
+     * 获取下拉选的数据
+     *
+     * @return key=AnField.OptionData.value（存到数据库中的数据）, value=AnField.OptionData.label（中文意思）
+     */
+    public static Map<Object, Object> getOptionData(AnField anField) {
+        HashMap<Object, Object> mapping = new HashMap<>();
+        String optionTypeSql = anField.getOptionTypeSql();
+        AnField.OptionAnnoClass optionAnnoClass = anField.getOptionAnnoClass();
+        if (StrUtil.isNotBlank(optionTypeSql)) {
+            try {
+                List<Map<String, Object>> mapList = DbContext.use(AnnoConstants.DEFAULT_DATASOURCE_NAME).sql(optionTypeSql).getDataList().getMapList();
+                for (Map<String, Object> map : mapList) {
+                    mapping.put(map.get("value").toString(), map.get("label"));
+                }
+            } catch (SQLException e) {
+                throw new BizException(e);
+            }
+        } else if (!optionAnnoClass.getAnnoClass().equals(Object.class)) {
+            AnEntity anEntity = AnnoBeanUtils.metadataManager().getEntity(optionAnnoClass.getAnnoClass());
+            List<?> dataList = AnnoBeanUtils.getBean(DbServiceWithProxy.class).list(
+                anEntity.getClazz(),
+                new ArrayList<>()
+            );
+            for (Object data : dataList) {
+                mapping.put(
+                    ReflectUtil.getFieldValue(data, optionAnnoClass.getIdKey()),
+                    ReflectUtil.getFieldValue(data, optionAnnoClass.getLabelKey())
+                );
+            }
+        } else if (CollUtil.isNotEmpty(anField.getOptionDatas())) {
+            for (AnField.OptionData optionData : anField.getOptionDatas()) {
+                mapping.put(optionData.getValue(), optionData.getLabel());
+            }
+        }
+        return mapping;
+    }
+
+    public static List<Options.Option> getOptions(AnField anField) {
+        List<Options.Option> optionItemList = new ArrayList<>();
+        Map<Object, Object> optionData = getOptionData(anField);
+        for (Map.Entry<Object, Object> entry : optionData.entrySet()) {
+            Options.Option option = new Options.Option();
+            option.setLabel(StrUtil.toString(entry.getValue()));
+            option.setValue(StrUtil.toString(entry.getKey()));
+            optionItemList.add(option);
+        }
+        return optionItemList;
     }
 
 
