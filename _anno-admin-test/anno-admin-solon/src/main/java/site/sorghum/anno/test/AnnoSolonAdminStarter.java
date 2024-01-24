56c1bc29b21e6350ec3170d7285976dcd15f316d
songyinyin <song_yinyin@163.com>
2024-01-24 22:36:51 +0800

    style: 导包

--- _anno-admin-test/anno-admin-solon/src/main/java/site/sorghum/anno/test/AnnoSolonAdminStarter.java
+++ _anno-admin-test/anno-admin-solon/src/main/java/site/sorghum/anno/test/AnnoSolonAdminStarter.java
@@ -1,16 +1,11 @@
 package site.sorghum.anno.test;
 
-import cn.dev33.satoken.stp.StpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.noear.solon.Solon;
-import org.noear.solon.SolonApp;
 import org.noear.solon.annotation.SolonMain;
 import org.noear.solon.scheduling.annotation.EnableRetry;
 import org.noear.solon.scheduling.annotation.EnableScheduling;
-import org.noear.wood.WoodConfig;
 import site.sorghum.anno.anno.annotation.global.AnnoScan;
-import site.sorghum.anno.anno.interfaces.CheckPermissionFunction;
-import site.sorghum.anno.solon.interceptor.WoodSqlLogInterceptor;
 
 /**
  * Ano 管理入门
