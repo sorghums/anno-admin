f71f6ac854356c46f054d6de0b457259f9bef369
songyinyin <song_yinyin@163.com>
2024-03-08 14:51:49 +0800

    fix(spring): SpringBoot 配置 cors

--- anno-admin-spring-boot-starter/src/main/java/site/sorghum/anno/spring/config/SaConfig.java
+++ anno-admin-spring-boot-starter/src/main/java/site/sorghum/anno/spring/config/SaConfig.java
@@ -4,9 +4,13 @@ import cn.dev33.satoken.dao.SaTokenDao;
 import cn.dev33.satoken.dao.SaTokenDaoOfRedis;
 import cn.dev33.satoken.interceptor.SaInterceptor;
 import org.noear.redisx.RedisClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.sorghum.anno._common.AnnoConstants;
@@ -51,6 +55,21 @@ public class SaConfig implements WebMvcConfigurer {
             .excludePathPatterns(AnnoConstants.BASE_URL + "/logo.png")
             .excludePathPatterns(AnnoConstants.BASE_URL + "/assets/**")
             .excludePathPatterns(AnnoConstants.BASE_URL + "/index.html");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许所有域名跨域访问
+        config.addAllowedOrigin("*");
+        // 允许跨域请求的方法
+        config.addAllowedMethod("*");
+        // 允许跨域请求包含的头信息
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
