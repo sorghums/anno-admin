53b3b8b960b6df8952f6dc3c656ae5bf8cd004b3
Sorghum <sorghum2021@163.com>
2024-04-23 16:28:47 +0800

    fix(项目): 修复原有旧代理类支持.

--- anno-admin/src/main/java/site/sorghum/anno/method/route/EntityMethodRoute.java
+++ anno-admin/src/main/java/site/sorghum/anno/method/route/EntityMethodRoute.java
@@ -4,11 +4,14 @@ import cn.hutool.core.util.ClassUtil;
 import site.sorghum.anno.db.DbCriteria;
 import site.sorghum.anno.method.MTContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author songyinyin
  * @since 2024/1/17 10:37
  */
-public class EntityMethodRoute implements MethodRoute{
+public class EntityMethodRoute implements MethodRoute {
     @Override
     public String[] route(MTContext context) {
         Object arg = context.getArgs()[0];
@@ -16,7 +19,18 @@ public class EntityMethodRoute implements MethodRoute{
         if (arg instanceof DbCriteria) {
             subPath = ((DbCriteria) arg).getEntityName();
         } else if (!ClassUtil.isJdkClass(arg.getClass())) {
-            subPath = arg.getClass().getSimpleName();
+            List<String> route = new ArrayList<>();
+            Class<?> argClass = arg.getClass();
+            // 循环直到父类是Object
+            while (true) {
+                if (argClass.getSuperclass() == Object.class) {
+                    break;
+                }
+                route.add(argClass.getSimpleName());
+                argClass = argClass.getSuperclass();
+            }
+            route.add(0, "all");
+            return route.toArray(new String[0]);
         }
         if (subPath == null) {
             return new String[]{"all"};
