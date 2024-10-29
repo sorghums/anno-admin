28f7cbd517076c37a9a1221b6e50665150f8dcce
Sorghum <sorghum2021@163.com>
2024-10-29 15:40:27 +0800

    feat: 完善逻辑@Ext适用于外置插件@Com适用于内置插件.

--- anno-admin/src/main/java/site/sorghum/anno/pf4j/Pf4jPluginManager.java
+++ anno-admin/src/main/java/site/sorghum/anno/pf4j/Pf4jPluginManager.java
@@ -4,13 +4,17 @@ import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.pf4j.*;
 import site.sorghum.anno._common.AnnoBeanUtils;
+import site.sorghum.anno.plugin.AnnoPlugin;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Slf4j
 public class Pf4jPluginManager extends DefaultPluginManager {
+    // getExtensions 是否初始化过
+    private volatile boolean init = false;
 
     public Pf4jPluginManager() {
     }
@@ -31,6 +35,12 @@ public class Pf4jPluginManager extends DefaultPluginManager {
     @Override
     protected <T> List<T> getExtensions(List<ExtensionWrapper<T>> extensionsWrapper) {
         List<T> extensions = new ArrayList<>(extensionsWrapper.size());
+        // 兼容旧版本 @Component 注解
+        if (!init && !extensionsWrapper.isEmpty() && extensionsWrapper.get(0).getExtension() instanceof AnnoPlugin) {
+            List<AnnoPlugin> beans = AnnoBeanUtils.getBeansOfType(AnnoPlugin.class);
+            extensions.addAll((Collection<? extends T>) beans);
+            init = true;
+        }
         for (ExtensionWrapper<T> extensionWrapper : extensionsWrapper) {
             try {
                 extensions.add(extensionWrapper.getExtension());
@@ -40,6 +50,10 @@ public class Pf4jPluginManager extends DefaultPluginManager {
         }
         for (T extension : extensions) {
             String beanName = StrUtil.lowerFirst(extension.getClass().getSimpleName());
+            Object bean = AnnoBeanUtils.getBean(beanName);
+            if (bean != null) {
+                continue;
+            }
             AnnoBeanUtils.registerBean(beanName, extension);
         }
         return extensions;
