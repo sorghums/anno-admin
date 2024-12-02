a6cf4521f021c0607c344604f5108c3ce3ade975
Sorghum <sorghum2021@163.com>
2024-12-02 16:38:26 +0800

    warm-flow flow-designer add HandlerSelectServiceImpl.java.

---
+++ anno-admin-platform/src/main/java/site/sorghum/anno/plugin/dao/AnOrgDao.java
@@ -0,0 +1,9 @@
+package site.sorghum.anno.plugin.dao;
+
+import jakarta.inject.Named;
+import site.sorghum.anno.db.dao.AnnoBaseDao;
+import site.sorghum.anno.plugin.ao.AnOrg;
+
+@Named
+public class AnOrgDao implements AnnoBaseDao<AnOrg> {
+}
